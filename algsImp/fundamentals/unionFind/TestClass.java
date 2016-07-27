5f932cdb1b0f699c2856e3fc973f572e5b78352a
stan <stan@192.168.1.2>
2016-07-27 18:29:26 +0200

    First commit, second try

---
+++ algsImp/fundamentals/unionFind/TestClass.java
@@ -0,0 +1,14 @@
+package unionFind;
+
+public class TestClass {
+
+	public TestClass() {
+		System.out.println("This is a test class");
+	}
+	
+	public static void main(String[] args) {
+		TestClass tc = new TestClass();
+	}
+}
+
+
